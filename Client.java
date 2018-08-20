b416718a78f0bee7a87b7457b3f4fd7aa6e80455
Igor Smirnov <smirnov23@gmail.com>
2018-08-20 21:27:26 +0300

    Многопоточный сервер

--- Client.java
+++ Client.java
@@ -7,22 +7,22 @@ class Client
 	{
 	    Socket socket = new Socket("localhost", 1500);
 		
-		BufferedReader inStd = new BufferedReader(new InputStreamReader(System.in));
+		BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
 		BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 		PrintStream out = new PrintStream(socket.getOutputStream());
 		
 		while (true) {
-		    String str = inStd.readLine();
+		    String str = stdin.readLine();
 		
 		    out.println(str);
-		    str = in.readLine();
-
-		    System.out.println(str);
 			if (str.equals("exit")) {
 				break;
+			} else {
+		        str = in.readLine();
+		        System.out.println(str);
 			}
 		}
-		
+		System.out.println("Client work done.");
 		socket.close();
 	}
 }
--- Server.java
+++ Server.java
@@ -1,6 +1,43 @@
 import java.io.*;
 import java.net.*;
 
+class Connection implements Runnable
+{
+	private Thread thread;
+	private Socket clientSocket;
+	
+	Connection(Socket client)
+	{
+		clientSocket = client;
+		thread = new Thread(this);
+		thread.start();
+	}
+	
+	public void run()
+	{
+		try {
+ 		    BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+		    PrintStream out = new PrintStream(clientSocket.getOutputStream());
+		    while (true) {
+			    String str = in.readLine();
+			    System.out.println("Received message: " + str);
+			    out.println("Message received.");
+			    if (str.equals("exit")) {
+				    break;
+			    }
+		    }
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		} finally {
+			try {
+		        clientSocket.close();
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+			}				
+		}
+	}
+}
+
 class Server
 {
     public static void main (String[] args) throws IOException
@@ -10,20 +47,7 @@ class Server
 		while (true) {
 		    Socket clientSocket = serverSocket.accept();
 		    System.out.println("Accepted: " + clientSocket);
-		
-		    BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-		    PrintStream out = new PrintStream(clientSocket.getOutputStream());
-		
-		    String str = in.readLine();
-		
-		    System.out.println(str);
-		
-			if (str.equals("exit")) {
-		        out.println("exit");
-		        clientSocket.close();
-			} else {
-		        out.println("Message received.");
-			}
+			Connection conn = new Connection(clientSocket);
 		}
 	
 		//serverSocket.close();
